2aa9542183d66996ed62d84e50ec2481d06a40b8
TJP <69581225+JavaProgswing@users.noreply.github.com>
2025-11-24 19:15:51 +0530

    4.02 Referral added!

---
+++ src/main/java/com/jprcoder/valnarratorbackend/ReferralNotification.java
@@ -0,0 +1,9 @@
+package com.jprcoder.valnarratorbackend;
+
+public class ReferralNotification {
+    public long id;
+    public String user_id;
+    public String referred_user_id;
+    public long bonus_duration;
+    public long created_at;
+}
---
+++ src/main/java/com/jprcoder/valnarratorbackend/ReferralNotificationsResponse.java
@@ -0,0 +1,7 @@
+package com.jprcoder.valnarratorbackend;
+
+import java.util.List;
+
+public class ReferralNotificationsResponse {
+    public List<ReferralNotification> notifications;
+}
